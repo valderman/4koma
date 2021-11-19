f8911a8cfbfd58cd0de3473fff161f8555fcbbd2
Anton Ekblad <anton@ekblad.cc>
2021-11-19 01:43:26 +0100

    Mark Generated annotation as internal.

--- src/main/kotlin/cc/ekblad/toml/JacocoUtils.kt
+++ src/main/kotlin/cc/ekblad/toml/JacocoUtils.kt
@@ -5,4 +5,4 @@ package cc.ekblad.toml
  * Tells JaCoCo to disregard the annotated item when reporting code coverage.
  * Needed to avoid false negative coverage scares as JaCoCo doesn't understand Kotlin inline functions.
  */
-annotation class Generated
+internal annotation class Generated
